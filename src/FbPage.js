947760fcdb849e464242b083c74be0d3ff077809
maryam032 <maryamakram032@gmail.com>
2019-07-22 09:40:42 +0500

    Font adjusted

--- src/FbPage.js
+++ src/FbPage.js
@@ -5,49 +5,53 @@ class FbPage extends React.Component{
     render(){
         return(
             <div>
-                <div style={{border:5,borderColor:'black',width:1360,height:100,backgroundColor:'#3b5998'}}>
+                <div style={{float:'left',border:5,borderColor:'black',width:1350,height:80,backgroundColor:'#3b5998'}}>
                 
-                <h1 style={{float:'left',color:'white' ,marginLeft:160}}>facebook</h1>
-                <div style={{width:450,height:100,backgroundColor:'#3b5998',marginLeft:750,}}>
+                <h1 style={{float:'left',color:'white' ,marginLeft:180,fontFamily:'Helvetica',fontSize:40}}>facebook</h1>
+                <div style={{width:450,height:80,backgroundColor:'#3b5998',marginLeft:750,}}>
                     <div style={{float:'left'}}>
-                  <p style={{outline:'none',marginBottom:2,marginLeft:5,marginTop:15,color:'white'}}>Enter email</p>
-                  <input style={{float:'left',marginLeft:5,}}  type="email"  placeholder="Email"></input>
+                  <p style={{outline:'none',marginBottom:2,marginLeft:5,marginTop:13,color:'white'}}>Enter email</p>
+                  <input style={{float:'left',marginLeft:5,height:15}}  type="email"  placeholder="Email"></input>
                 </div>
                 <div style={{float:'left',marginLeft:15}}>
                     <div style={{float:'left'}}>
-                  <p style={{outline:'none',marginBottom:2,color:'white'}}>Enter Password</p>
-                  <input style={{float:'right',marginRight:10}}  type="password"  placeholder="Password"  ></input>
+                  <p style={{outline:'none',marginBottom:2,color:'white',marginTop:13}}>Enter Password</p>
+                  <input style={{float:'right',marginRight:10,height:15}}  type="password"  placeholder="Password"  ></input>
                   </div>
-                <input style={{float:'right',marginTop:40}} type="button" value="Login"/>
+                <input className="button" style={{float:'right',marginTop:35}} type="button" value="Log In"/>
                 </div>
-                <div>
-                    <div style={{float:'left'}}><input type="checkbox"/>
-                     <span style={{color:'white'}}>Remember me </span>
+                <div >
+                    <div style={{float:'left',marginTop:0}}><input type="checkbox"/>
+                     <span style={{color:'white',fontSize:14}}>Remember me </span>
                      
                      </div>
-                    <div style={{float:'left',marginLeft:68,color:'white'}}><a href="">Forgot Password ?</a></div>
+                    <div style={{float:'left',marginLeft:82,color:'white',marginTop:0}}><span style={{color:'#9fa8cb',fontSize:14}}>Forgot Password ?</span></div>
                 </div>
                 </div>
                 </div>
                
 
-
-                <div className="grad" style={{float:'left',border:'2px solid black',width:664,height:450,backgroundColor:'#dfe3ee '}}>
-                  <p style={{marginLeft:70,fontSize:30,fontWeight:'bold',color:'#44799B  '}}>Facebook helps you connect and share with the people in your life.</p>
+                <div>
+                <div className="grad" style={{float:'left',width:664,height:510,backgroundColor:'#dfe3ee '}}>
+                  <p style={{marginLeft:180,fontSize:20,fontWeight:'bold',color:'#2b3c63',fontFamily:'Arial',marginTop:40}}>Facebook helps you connect and share with the people in your life.</p>
+                  <img style={{marginTop:18,outline:'none',display:'block',width:"490px" ,marginLeft:180}}src={process.env.PUBLIC_URL +"/facebookpage.png.png"}
+                 alt="Mypic" />
                 </div>
                 
 
-
-                <div className="grad" style={{float:'right',border:'2px solid black',paddingLeft:25,width:669,height:450,backgroundColor:'#dfe3ee'}}>
-                    <div style={{float:'left',width:694,height:50,}}>
-                   <p style={{fontSize:30,color:'black',fontWeight:'bolder',fontFamily:'serif',marginTop:20}}>Create an account</p></div>
-                   <div style={{float:'left',width:694,height:30}}>
-                   <p style={{color:'black',fontSize:15,marginTop:8,fontFamily:'serif'}}> It's free and always will be.</p></div>
-                   
-                   <input  style={{float:'left',width:160,marginTop:20,borderRadius:5,border:'none',outline:'none',height:35,paddingLeft:10}} type="text" placeholder="First Name"></input>
-                   <input  style={{marginRight:300,float:'right',width:150,marginTop:20,borderRadius:5,border:'none',outline:'none',height:35,paddingLeft:10}} type="text" placeholder="Last Name"></input>
-                   <input  style={{marginRight:300,float:'left',width:360,marginTop:20,borderRadius:5,border:'none',outline:'none',height:35,paddingLeft:10}} type="email "placeholder="Mobile number or email adress"></input>
-                   <input  style={{marginRight:300,float:'left',width:360,marginTop:20,borderRadius:5,border:'none',outline:'none',height:35,paddingLeft:10,size:20}} type="password "placeholder="New password"></input>
+                <div  className="grad"style={{backgroundColor:'#dfe3ee',width:1350,height:612}}>
+                        <div className="grad" style={{float:'right',paddingLeft:25,width:635,height:510,backgroundColor:'#dfe3ee',marginLeft:0}}>
+                        <div style={{float:'left',width:620,height:50,}}>
+                    <p style={{fontSize:35,color:'black',fontWeight:'bolder',fontFamily:'Tahoma',marginTop:20}}>Create an account</p></div>
+                    <div style={{float:'left',width:620,height:30}}>
+                    <p style={{color:'black',fontSize:15,marginTop:15,fontFamily:'Arial',marginLeft:10}}> It's free and always will be.</p></div>
+                    
+                    <input  style={{border:'1px solid #8b9dc3',float:'left',width:160,marginTop:20,borderRadius:5,outline:'none',height:35,paddingLeft:10}} type="text" placeholder="First Name"></input>
+                    <input  style={{border:'1px solid #8b9dc3',marginRight:268,float:'right',width:160,marginTop:20,borderRadius:5,outline:'none',height:35,paddingLeft:10}} type="text" placeholder="Last Name"></input>
+                    <input  style={{border:'1px solid #8b9dc3',marginRight:300,float:'left',width:360,marginTop:20,borderRadius:5,outline:'none',height:35,paddingLeft:10}} type="email "placeholder="Mobile number or email adress"></input>
+                    <input  style={{border:'1px solid #8b9dc3',marginRight:300,float:'left',width:360,marginTop:20,borderRadius:5,outline:'none',height:35,paddingLeft:10,size:20}} type="password "placeholder="New password"></input>
+                    </div>
+                </div>
                 </div>
             </div>
         )
